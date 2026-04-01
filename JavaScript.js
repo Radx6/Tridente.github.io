9281f4cb25aad3e8586b4f916394325b825de7f9
Radx6 <markowskipawel393@gmail.com>
2026-04-01 20:52:28 +0200

    git push

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const audio = document.getElementById("audio");
 const btn = document.getElementById("playBtn");
 const progress = document.getElementById("progress");
@@ -19,3 +20,26 @@ audio.ontimeupdate = () => {
 progress.oninput = () => {
   audio.currentTime = (progress.value / 100) * audio.duration;
 };
+=======
+const audio = document.getElementById("audio");
+const btn = document.getElementById("playBtn");
+const progress = document.getElementById("progress");
+
+function togglePlay() {
+  if (audio.paused) {
+    audio.play();
+    btn.innerText = "⏸";
+  } else {
+    audio.pause();
+    btn.innerText = "▶";
+  }
+}
+
+audio.ontimeupdate = () => {
+  progress.value = (audio.currentTime / audio.duration) * 100;
+};
+
+progress.oninput = () => {
+  audio.currentTime = (progress.value / 100) * audio.duration;
+};
+>>>>>>> ca882b3 (git push)
